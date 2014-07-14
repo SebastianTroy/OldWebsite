a2fd8cbb9a73aba561b147f6499263672f72b1f0
Sebastian Troy <sebastiantroy@btinternet.com>
2014-07-14 13:45:08 +0100

    Strange egit bug stopped me commiting these with previous 'moved from old laptop'

---
+++ Website/who_am_i.php
@@ -0,0 +1,15 @@
+<?php
+
+$PageTitle = "Who am I?";
+
+function customPageHeader(){
+?>
+<link rel="stylesheet" type="text/css" href="css/basic_style.css">
+<?php }
+
+	include_once ('common/header.php');
+
+	include_once ('html/who_am_i.html');
+
+	include_once ('common/footer.php');
+?>
