a35006123f45c9ab427ae633ed9e9356452b0a7f
Sebastian Troy <sebastiantroy@btinternet.com>
2014-08-16 19:37:02 +0100

    -Moved fluid dynamics to its own sub page

---
+++ Website/projects/fluid-dynamics/fluid-dynamics.php
@@ -0,0 +1,19 @@
+<?php
+$PageTitle = "Modelling an incompressible fluid";
+function customCSSHeader() {
+	?>
+<!-- include css here, e.g. <link rel="stylesheet" type="text/css" href="css/stylesheet.css"> -->
+<?php
+}
+function customJavascriptHeader() {
+	?>
+<!-- include css here, e.g. <link rel="stylesheet" type="text/css" href="css/stylesheet.css"> -->
+<?php
+}
+
+include_once ('../../common/header.php');
+
+include_once ('fluid-dynamics.html');
+
+include_once ('../../common/footer.php');
+?>
