c50c6b06ce3b29970f9daf8c233a2a3286c3dbb6
Sebastian Troy <sebastiantroy@btinternet.com>
2014-08-20 16:59:48 +0100

    -Included all .jars on the website so that updates to github don't force information on the site to become outdated -Included legacy versions of programs where applicable -Fixed missing information on salesman page -Fixed unfinished sentence in project overview -Started implementing page for Evolving Plants

---
+++ Website/projects/evolving-plants/evovling-plants.php
@@ -0,0 +1,19 @@
+<?php
+$PageTitle = "Evolving Plants Simulation";
+function customCSSHeader() {
+	?>
+<!-- include css here, e.g. <link rel="stylesheet" type="text/css" href="css/stylesheet.css"> -->
+<?php
+}
+function customJavascriptHeader() {
+	?>
+<!-- include css here, e.g. <link rel="stylesheet" type="text/css" href="css/stylesheet.css"> -->
+<?php
+}
+
+include_once ('../../common/header.php');
+
+include_once ('evolving-plants.html');
+
+include_once ('../../common/footer.php');
+?>
