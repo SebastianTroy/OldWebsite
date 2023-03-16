508e5738245c0bd008279b42ab43badacd4971ea
Sebastian Troy <sebastian@troymail.uk>
2023-03-16 19:06:16 +0000

    Updated to allow for static hosting  - Removed wildly out of date CV  - Updated landing and contact pages to reflect the fact that this site is no longer maintained

--- Website/common/footer.php
+++
@@ -1,9 +0,0 @@
-
-</div>
-<!-- close page_container -->
-<footer id="footer_container"> Copyright&copy; Sebastian Troy 2020 </footer>
-<!-- close footer_container -->
-</div>
-<!-- close master_container -->
-</body>
-</html>
--- Website/contact/php/send_email.php
+++
@@ -1,9 +0,0 @@
-<?php
-$from = $_POST ["from"];
-$subject = $_POST ["subject"];
-$message = $_POST ["message"];
-// message lines should not exceed 70 characters (PHP rule), so wrap it
-$message = wordwrap ( $message, 70 );
-// send email
-mail ( "sebastiantroy@btinternet.com", "TroyDev - $subject", $message, "From: $from\n" );
-?>
