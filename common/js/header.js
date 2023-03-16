76bf7bbe33d879de499bea94453e0cb6d05afdbb
Sebastian Troy <sebastian@troymail.uk>
2023-03-16 19:15:12 +0000

    Moved files to root directory so github pages works as intended  - Updated gitignore which didn't contain any useful rules anymore

---
+++ common/js/header.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    // Set header to fixed
+    $(".nav_menu").css("position", "fixed");
+    // Make sure header and menu buttons correct size
+    calculate_header_size();
+    // This function will be executed when the user resizes the page.
+    $(window).on("resize", function () {
+        // Make sure header is correct size
+        calculate_header_size();
+    });
+    // This function will be executed when the user scrolls the page.
+    $(window).on("scroll", function () {
+        adjust_header();
+    });
+    // This code checks which tab is active and sets its background colour
+    var path = window.location.pathname.split("/").pop();
+    switch (path) {
+        case "":
+        case "index.html":
+            $(".nav_menu_button.about").css("background-color", "#aaa271");
+            break;
+        case "projects.html":
+            $(".nav_menu_button.projects").css("background-color", "#aaa271");
+            break;
+        case "contact.html":
+            $(".nav_menu_button.contact").css("background-color", "#aaa271");
+            break;
+    }
+
+    // When a menu button is pressed, highlight it & save its index in a cookie so it can be highlighted on the next page
+    $(".nav_menu_button").on("click", function () {
+        $(".nav_menu_button").css("background-color", "#C6C0A0");
+        $(this).css("background-color", "#AAA271");
+        $.cookie("tab", $(this).index(), {
+            path: "/",
+        });
+    });
+});
+
+// Calculate size header should be based on screen width
+function calculate_header_size() {
+    var text_size = Math.min($(window).width() / 6, 100) + "%";
+    $("#header_container").css("font-size", text_size);
+    adjust_page_padding();
+}
+
+// Make sure padding at top of page matches height of header
+function adjust_page_padding() {
+    $("#page_container").css("margin-top", function () {
+        return $(".nav_menu").outerHeight();
+    });
+}
+
+function adjust_header() {
+    // Once the user has scrolled down, hide the title of the header
+    if ($(window).scrollTop() < $(".title").position().top + $(".title").outerHeight()) {
+        $(".nav_menu").css("top", function () {
+            return $(".title").position().top + $(".title").outerHeight() - $(window).scrollTop();
+        });
+    } else {
+        $(".nav_menu").css("top", "0px");
+    }
+}
---
+++ contact/js/mail.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$("#submit").click(function() {
+		$(".error").empty();
+		var emailReg = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;
+
+		var emailFromVal = $("#email").val();
+		if (emailFromVal == '' || !emailReg.test(emailFromVal)) {
+			$(".email").css('display', 'block');
+			$(".email").html("Please enter a valid email adress");
+			return;
+		}
+
+		var messageVal = $("#message").val();
+		if (messageVal == '') {
+			$(".message").css('display', 'block');
+			$(".message").html("Please enter a message");
+			return;
+		}
+
+		jQuery.ajax({
+			type : "POST",
+			url : 'contact/php/send_email.php',
+			dataType : 'text',
+			data : {
+				from : $("#email").val(),
+				subject : $("#subject").val(),
+				message : $("#message").val()
+			},
+		});
+		
+		$(".email").css('display', 'none');
+		$(".message").css('display', 'none');
+		
+		$("#email").val('');
+		$("#subject").val('Feedback');
+		$("#message").val('');
+		alert("sent");
+	});
+});
